d6a9658c609abddd4c2770610448e5bd63b66f3f
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-03-21 22:51:31 +0600

    input field cursor focus added

--- src/components/RefForm/RefForm.jsx
+++ src/components/RefForm/RefForm.jsx
@@ -1,21 +1,29 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const RefForm = () => {
 
     const nameRef = useRef(null);
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    useEffect(() => {
+        nameRef.current.focus();
+    }, [])
 
     const handleSubmit = e => {
         e.preventDefault();
-        console.log(nameRef.current.value)
+        console.log(nameRef.current.value);
+        console.log(emailRef.current.value);
+        console.log(passwordRef.current.value);
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input ref={nameRef} type="text" name="name" />
                 <br />
-                <input type="email" name="email" id="" />
+                <input ref={emailRef} type="email" name="email" id="2" />
                 <br />
-                <input type="text" name="phone" id="" />
+                <input ref={passwordRef} type="password" name="password" id="" />
                 <br />
                 <input type="submit" value="Submit" />
             </form>
